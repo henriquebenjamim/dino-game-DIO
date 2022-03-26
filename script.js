da6c4a95878feccc855779cb21de4f90e8615367
henriquebenjamim <henriiqqq@gmail.com>
2022-03-25 23:39:10 -0300

    Adding gameOver colision

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 
 function handleKeyUp(event) {
@@ -12,8 +13,7 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-  let position = 0;
-
+  
   isJumping = true;
 
   let upInterval = setInterval(() => {
@@ -51,7 +51,12 @@ function createCactus() {
     if (cactusPosition < -60) {
       clearInterval(leftInterval);
       background.removeChild(cactus);
-    } else {
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // game over
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+    } 
+    else {
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + 'px';
     }
