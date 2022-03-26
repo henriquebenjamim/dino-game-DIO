43d7e54d374f7c55248922d712f1b2b09b1fb76d
henriquebenjamim <henriiqqq@gmail.com>
2022-03-25 23:28:21 -0300

    cactus movement

--- script.js
+++ script.js
@@ -37,13 +37,28 @@ function jump() {
   }, 20);
 }
 
-function createCatus() {
+function createCactus() {
   const cactus = document.createElement('div');
   let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
 
   cactus.classList.add('cactus');
   cactus.style.left = 1000 + 'px';
   background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  // delay da funcao
+  setTimeout(createCactus, randomTime);
 }
 
 createCactus();
