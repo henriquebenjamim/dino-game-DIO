e5f661aac53813b4caf0d1fd5babafaa1087c630
henriquebenjamim <henriiqqq@gmail.com>
2022-03-25 22:51:49 -0300

    adding script to know when the 'space' is used

---
+++ script.js
@@ -0,0 +1,10 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32)
+  {
+    console.log('Pressionou espaço!');
+  }
+}
+
+document.addEventListener('keyup', handleKeyUp);
