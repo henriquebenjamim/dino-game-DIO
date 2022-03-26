f85a919e571d3416af1042f1ab81a61ecda13638
henriquebenjamim <henriiqqq@gmail.com>
2022-03-25 23:02:21 -0300

    Adding dino jump function

--- script.js
+++ script.js
@@ -2,9 +2,33 @@ const dino = document.querySelector('.dino');
 
 function handleKeyUp(event) {
   if (event.keyCode === 32)
-  {
+  { 
+    jump();
     console.log('Pressionou espaço!');
   }
+}
+
+function jump() {
+  let position = 0;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+       clearInterval(upInterval);
+
+       let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval)
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+        
+       }, 20);
+    } else {
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
